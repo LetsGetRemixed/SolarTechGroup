537edcb125074a1694a7c6bd82763ced2df405de
LetsGetRemixed <73035961+LetsGetRemixed@users.noreply.github.com>
2024-09-05 20:36:25 -0500

    left out a save
    oops

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Components/Pages/Home';
+import Home from './Components/Pages/HomeComp/Home';
 
 function App() {
   return (
