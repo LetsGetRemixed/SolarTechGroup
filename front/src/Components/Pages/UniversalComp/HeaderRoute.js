9007684fefc31d152d1115cc07844b147fab4e1b
LetsGetRemixed <73035961+LetsGetRemixed@users.noreply.github.com>
2024-09-10 01:49:03 -0500

    small change to header
    bottom of image change

--- front/src/Components/Pages/UniversalComp/HeaderRoute.js
+++ front/src/Components/Pages/UniversalComp/HeaderRoute.js
@@ -15,7 +15,7 @@ const HeaderRoute = ({ imageUrl, title }) => {
 
                 {/* Breadcrumb Navigation */}
                 <div className="absolute bottom-0 left-0 w-full z-20">
-                    <div className="container mx-auto py-2 w-full max-w-none">
+                    <div className="container mx-auto py-0 w-full max-w-none">
                         <div className="text-md text-gray-200 bg-black bg-opacity-50 text-center px-4 sm:py-4 py-2 rounded-md">
                             <Link to="/" className="text-highlight hover:underline">Home</Link>
                             {breadcrumbs.length > 0 && breadcrumbs.map((crumb, index) => {
