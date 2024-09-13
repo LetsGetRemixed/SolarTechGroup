458b9980a45efbdf8c53a95b8ec9fca993a0b8ba
Isaiah Gage <frb32@txstate.edu>
2024-09-13 00:15:00 -0500

    Fixed issues - Holy Crap

--- Backend/models/admin.js
+++ Backend/models/admin.js
@@ -1,23 +1,32 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const argon2 = require('argon2');
 
 const adminSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
 
-// Hash password before saving the admin to the database
+// Hash password before saving the admin to the database 
 adminSchema.pre('save', async function (next) {
     if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 10);
+        try {
+            this.password = await argon2.hash(this.password);
+        } catch (error) {
+            next(error); // Pass the error to the next middleware
+        }
     }
     next();
 });
 
 // Compare password 
-adminSchema.methods.comparePassword = function (candidatepassword) {
-    return bcrypt.compare(candidatepassword, this.password);
+adminSchema.methods.comparePassword = async function (candidatePassword) {
+    try {
+        return await argon2.verify(this.password, candidatePassword);
+    } catch (error) {
+        throw new Error('Password verification failed');
+    }
 };
 
 const Admin = mongoose.model('Admin', adminSchema);
+
 module.exports = Admin;
